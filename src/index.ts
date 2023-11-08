62699e7b704f72f825e6056a3dbe60b70643fcf9
Sebastian H <s.holler@eprivacy.eu>
2023-11-08 17:25:27 +0100

    basic bot setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("IM A DUCK");
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+try {
+    require('dotenv').config()
+} catch (error) {
+    console.log(error);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c: any) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
